bed21989602e99950931bc141139c8606218dd70
KamenskiyIlya <raptor5501999@gmail.com>
2026-01-04 00:56:49 +0300

    Разкомментировал строки

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ rendered_page = template.render(
 with open('index.html', 'w', encoding='utf8') as file:
 	file.write(rendered_page)
 
-# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-# server.serve_forever()
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
 
 pprint(get_excel_data(), indent=4)
